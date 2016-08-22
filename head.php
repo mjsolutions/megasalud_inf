2509015b01654987577642d879e397159fccaf92
martinalanis <martinalanis.dev@gmail.com>
2016-08-22 17:18:03 -0500

    Terminado menu

---
+++ head.php
@@ -0,0 +1,155 @@
+<link href='https://fonts.googleapis.com/css?family=Raleway:400,300,500,700,900' rel='stylesheet' type='text/css'>
+<!-- FontAwesome CSS -->
+<link href="assets/fonts/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+<!-- Material Icons CSS -->
+<link href="assets/fonts/iconfont/material-icons.css" rel="stylesheet">
+<!-- magnific-popup -->
+<link href="assets/magnific-popup/magnific-popup.css" rel="stylesheet">
+<!-- owl.carousel -->
+<link href="assets/owl.carousel/assets/owl.carousel.css" rel="stylesheet">
+<link href="assets/owl.carousel/assets/owl.theme.default.min.css" rel="stylesheet">
+<!-- flexslider -->
+<link href="assets/flexSlider/flexslider.css" rel="stylesheet">
+<!-- materialize -->
+<link href="assets/materialize/css/materialize.min.css" rel="stylesheet">
+<!-- Bootstrap -->
+<link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<!-- shortcodes -->
+<link href="assets/css/shortcodes/shortcodes5e1f.css?v=2" rel="stylesheet">
+<!-- Style CSS -->
+<link href="style5e1f.css?v=2" rel="stylesheet">
+
+<!-- RS5.0 Main Stylesheet -->
+<link rel="stylesheet" type="text/css" href="assets/revolution/css/settings.css">
+<!-- RS5.0 Layers and Navigation Styles -->
+<link rel="stylesheet" type="text/css" href="assets/revolution/css/layers.css">
+<link rel="stylesheet" type="text/css" href="assets/revolution/css/navigation.css">
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+
+<body id="top" class="has-header-search">
+
+	<!--header start-->
+	<header id="header" class="tt-nav">
+
+		<div class="header-sticky light-header nav-boxed-color">
+
+			<div class="container">
+
+				<div id="matrox-menu" class="menuzord">
+
+					<!--logo start-->
+					<a href="index-2.html" class="logo-brand">
+						<img src="assets/img/logo-megasalud.jpg" alt="Megasalud"/>
+					</a>
+					<!--logo end-->
+
+					<!--mega menu start-->
+					<ul class="menuzord-menu pull-right">
+						<li class="active"><a href="javascript:void(0)">Megasalud</a>
+							<ul class="dropdown">
+								<li><a href="#">¿Quiénes Somos?</a></li>
+								<li><a href="#">Red de consultorios</a></li>
+								<li><a href="#">Médicos</a></li>								
+							</ul>
+						</li>
+
+						<li><a href="javascript:void(0)">Enfermedades Autoinmunes</a>
+							<div class="megamenu">
+								<div class="megamenu-row">
+									<div class="col3">
+										<!-- <h2>Menu Type Page</h2> -->
+										<ul class="list-unstyled clearfix">
+											<li><a href="">Enfermedades Autoinmunes</a></li>
+											<li><a href="">Diabetes</a></li>
+											<li><a href="">Cáncer</a></li>
+										</ul>
+									</div>
+									<div class="col3">
+										<!-- <h2>Menu Type Page</h2> -->
+										<ul class="list-unstyled clearfix">
+											<li><a href="">Sida</a></li>
+											<li><a href="">Vitiligo</a></li>
+											<li><a href="">Asma</a></li>
+										</ul>
+									</div>
+									<div class="col3">
+										<!-- <h2>Menu Type Page</h2> -->
+										<ul class="list-unstyled clearfix">
+											<li><a href="">Asma Infantil</a></li>
+											<li><a href="">Lupus</a></li>
+											<li><a href="">Esclerodermia</a></li>
+										</ul>
+									</div>
+									<div class="col3">
+										<!-- <h2>Menu Type Page</h2> -->
+										<ul class="list-unstyled clearfix">
+											<li><a href="">Psoriasis</a></li>
+											<li><a href="">Artritis</a></li>
+											<li><a href="" target="_blank">Otras</a></li>
+										</ul>
+									</div>
+
+								</div>
+							</div>
+						</li>
+
+						<li><a href="javascript:void(0)">Tratamiento</a>
+							<ul class="dropdown">
+								<li><a href="#">Método Vranfcua</a></li>
+								<li><a href="#">Hemovacuna</a></li>
+								<li><a href="#">Reyshen</a></li>								
+								<li><a href="#">Glicoxil</a></li>								
+								<li><a href="#">Testimonios</a></li>								
+								<li><a href="#">Animación</a></li>								
+							</ul>
+						</li>
+
+						<li><a href="javascript:void(0)">Ganoderma Lucidum</a>
+							<ul class="dropdown">
+								<li><a href="#">¿Qué es?</a></li>
+								<li><a href="#">Orígenes</a></li>
+								<li><a href="#">Beneficios</a></li>								
+								<li><a href="#">Composición química</a></li>								
+								<li><a href="#">Estudios científicos</a></li>								
+								<li><a href="#">Uso</a></li>								
+							</ul>
+						</li>
+
+						<li><a href="javascript:void(0)">Estilo de Vida</a>
+							<ul class="dropdown">
+								<li><a href="#">Estilo de vida</a></li>
+								<li><a href="#">Alimentación</a></li>
+								<li><a href="#">Ejercicio</a></li>								
+								<li><a href="#">Descanso</a></li>								
+								<li><a href="#">Tranquilidad</a></li>							
+							</ul>
+						</li>
+
+						<li><a href="javascript:void(0)">Tienda</a>
+							<ul class="dropdown">
+								<li><a href="#">Catálogo completo</a></li>
+								<li><a href="#">Línea Salud</a></li>
+								<li><a href="#">Línea Nutrición</a></li>								
+								<li><a href="#">Línea Belleza</a></li>								
+								<li><a href="#">Promociones</a></li>							
+							</ul>
+						</li>
+
+						<li><a href="javascript:void(0)">Contacto</a>
+
+						</li>
+					</ul>
+					<!--mega menu end-->
+
+				</div>
+			</div>
+		</div>
+	</header>
+<!--header end-->
