392ba5a93645510e9e77195778d2a143253742e0
Jaime Rodríguez <Jaime Rodrguez>
2016-09-01 17:59:09 -0500

    Landing Diabetes

--- diabetes.php
+++ diabetes.php
@@ -45,6 +45,72 @@
 </section>
 <!-- Content end -->
 
+        <section class="padding-bottom-100">
+            <div class="container">
+              <div class="text-center mb-80">
+                  <h2>Vertical Tab</h2>
+                  <p>Matrox has 8 unique tab style</p>
+              </div>
+
+              <div class="vertical-tab">
+                <div class="row">
+                  <div class="col-sm-3">
+                        <!-- Nav tabs -->
+                        <ul class="nav nav-tabs nav-stacked" role="tablist">
+                          <li role="presentation" class="active"><a href="#tab-5" class="waves-effect waves-light" role="tab" data-toggle="tab">Epidemiología</a></li>
+                          <li role="presentation"><a href="#tab-6" class="waves-effect waves-light" role="tab" data-toggle="tab">Diagnostico</a></li>
+                          <li role="presentation"><a href="#tab-7" class="waves-effect waves-light" role="tab" data-toggle="tab">Manifestaciones Clínicas</a></li>
+                          <li role="presentation"><a href="#tab-8" class="waves-effect waves-light" role="tab" data-toggle="tab">Posibles Complicaciones</a></li>
+                        </ul>                      
+                  </div><!-- /.col-md-3 -->
+
+                  <div class="col-sm-9">
+                        <!-- Tab panes -->
+                        <div class="panel-body">
+                          <div class="tab-content">
+                            <div role="tabpanel" class="tab-pane fade in active" id="tab-5">
+                                <h2>About Us</h2>
+
+                                <img class="alignright" src="assets/img/working_man.png" alt="">
+
+                                <p>La Diabetes Mellitus es una enfermedad muy frecuente y es un motivo de consulta médica con gran mortalidad actualmente. La forma más frecuente de Diabetes Mellitus es la tipo 2 representando un 90 a 95% del total de Diabetes y la frecuencia está aumentando muy rápidamente debido a los hábitos de vida que prevalecen actualmente y actualmente el 50% de los paciente diabéticos aun no han sido diagnosticados debido a su carácter silente, y 20% de estos presenta ya desafortunadamente complicaciones. Se estima que dentro de 10 años de duplicará el número de pacientes diabéticos tanto de tipo 1 como de tipo 2. Se estima que sólo en México, hay alrededor de 10 millones de diabéticos, es un problema de salud nacional.</p>
+                            </div>
+                            <div role="tabpanel" class="tab-pane fade" id="tab-6">
+                                <h2>What We Do</h2>
+
+                                <img class="alignright" src="assets/img/busy_man.png" alt="">
+
+                                <p>Duis senectus mus condimentum nunc ac habitasse duis consectetur a arcu a accumsan cras et metus ultricies justo cum a bibendum. <a href="#">Egestas vestibulum blandit sem vestibulum curabitur</a> a vel aliquet gravida ullamcorper amet dictumst vestibulum a elementum proin id volutpat magna parturient.</p>
+
+                                <p>Himenaeos a vestibulum morbi. <a href="#">Ullamcorper cras scelerisque</a> taciti lorem metus feugiat est lacinia facilisis id nam leo condimentum praesent id diam. Vestibulum amet porta odio elementum convallis parturient tempor tortor.</p>
+                            </div>
+                            <div role="tabpanel" class="tab-pane fade" id="tab-7">
+                                <h2>Our Mission</h2>
+
+                                <img class="alignright" src="assets/img/mission.png" alt="">
+
+                                <p>Duis senectus mus condimentum nunc ac habitasse duis consectetur a arcu a accumsan cras et metus ultricies justo cum a bibendum. <a href="#">Egestas vestibulum blandit sem vestibulum curabitur</a> a vel aliquet gravida ullamcorper amet dictumst vestibulum a elementum proin id volutpat magna parturient.</p>
+
+                                <p>Himenaeos a vestibulum morbi. <a href="#">Ullamcorper cras scelerisque</a> taciti lorem metus feugiat est lacinia facilisis id nam leo condimentum praesent id diam. Vestibulum amet porta odio elementum convallis parturient tempor tortor.</p>
+                            </div>
+                            <div role="tabpanel" class="tab-pane fade" id="tab-8">
+                                <h2>Our Team</h2>
+
+                                <img class="alignright" src="assets/img/business.png" alt="">
+
+                                <p>Duis senectus mus condimentum nunc ac habitasse duis consectetur a arcu a accumsan cras et metus ultricies justo cum a bibendum. <a href="#">Egestas vestibulum blandit sem vestibulum curabitur</a> a vel aliquet gravida ullamcorper amet dictumst vestibulum a elementum proin id volutpat magna parturient.</p>
+
+                                <p>Himenaeos a vestibulum morbi. <a href="#">Ullamcorper cras scelerisque</a> taciti lorem metus feugiat est lacinia facilisis id nam leo condimentum praesent id diam. Vestibulum amet porta odio elementum convallis parturient tempor tortor.</p>
+                            </div>
+                          </div>
+                        </div>
+                  </div>
+                </div><!-- /.row -->
+              </div><!-- /.vertical-tab -->
+
+            </div><!-- /.container -->
+        </section>
+
 <?php include ('footer.php'); ?>
 
 <!-- jQuery -->
